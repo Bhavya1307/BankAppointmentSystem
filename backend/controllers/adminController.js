65d6440e259ee3cc6e80545aa0da904e698b1746
Bhavya <bdp9834@gmail.com>
2024-10-03 12:12:54 -0400

    Backend added

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,10 @@
+// api for adding employee
+const addEmp = async (req,res) => {
+    try {
+        const { name, email, passsword, speciality, position, degree, experience, about, address } = req.body
+    } catch (error) {
+        
+    }
+}
+
+export {addEmp}

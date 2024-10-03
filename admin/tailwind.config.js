87f952d0314c772928759e3d3418719a421d529c
Bhavya <bdp9834@gmail.com>
2024-10-03 16:47:02 -0400

    Admin side added

--- admin/tailwind.config.js
+++ admin/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary': "rgb(65, 189, 65)"
+      }
+    },
   },
   plugins: [],
 }

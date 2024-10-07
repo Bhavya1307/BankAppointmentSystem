3b7fdc6be62a70e32aa9fcbb650ae3e1ff7740df
Bhavya <bdp9834@gmail.com>
2024-10-07 13:05:28 -0400

    Created apis for profile page

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,25 +2,26 @@ import validator from 'validator'
 import bycrypt from 'bcrypt'
 import userModel from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
+import {v2 as cloudinary} from 'cloudinary'
 
 // API to register user
-const registerUser = async (req,res) => {
+const registerUser = async (req, res) => {
     try {
         const { name, email, password } = req.body
-        
+
         // validating empty blocks
         if (!name || !password || !email) {
-            return res.json({success:false, message:"Missing details"})
+            return res.json({ success: false, message: "Missing details" })
         }
-        
+
         // validating email
         if (!validator.isEmail(email)) {
-            return res.json({success:false, message:"Invalid email"})
+            return res.json({ success: false, message: "Invalid email" })
         }
 
         // validating password
         if (password.length < 8) {
-            return res.json({success:false, message:"Enter a strong password"})
+            return res.json({ success: false, message: "Enter a strong password" })
         }
 
         // Hashing the password
@@ -30,46 +31,90 @@ const registerUser = async (req,res) => {
         const userData = {
             name,
             email,
-            password : hashedPassword
+            password: hashedPassword
         }
 
         const newUser = new userModel(userData)
         const user = await newUser.save()
 
-        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
 
-        res.json({success:true, token})
-        
+        res.json({ success: true, token })
+
     } catch (error) {
         console.log(error)
-        res.json({success:false, message:error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
 // API for user login
-const loginUser = async (req,res) => {
+const loginUser = async (req, res) => {
     try {
 
-        const {email, password} = req.body
-        const user = await userModel.findOne({email})
+        const { email, password } = req.body
+        const user = await userModel.findOne({ email })
 
         if (!user) {
-            return res.json({success:false, message:"User does not exist"})
+            return res.json({ success: false, message: "User does not exist" })
         }
 
         const isMatch = await bycrypt.compare(password, user.password)
 
         if (isMatch) {
-            const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
-            res.json({success:true,token})
+            const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+            res.json({ success: true, token })
         } else {
-            res.json({success:false,message:"Invalid credentials"})
+            res.json({ success: false, message: "Invalid credentials" })
         }
 
     } catch (error) {
         console.log(error)
-        res.json({success:false, message:error.message})
+        res.json({ success: false, message: error.message })
     }
 }
 
-export {registerUser, loginUser}
+// API to get user data
+const getProfile = async (req, res) => {
+    try {
+
+        const { userId } = req.body
+        const userData = await userModel.findById(userId).select('-password')
+
+        res.json({ success: true, userData })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+// API to update profile data
+const updateProfile = async (req, res) => {
+    try {
+
+        const { userId, name, phone, address, dob, gender } = req.body
+        const imageFile = req.file
+
+        if (!name || !phone || !dob || !gender) {
+            return res.json({ success: false, message: "Data Missing!" })
+        }
+
+        await userModel.findByIdAndUpdate(userId, { name, phone, address: JSON.parse(address), dob, gender })
+
+        if (imageFile) {
+            // uploading image to cloudinary
+            const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type:'image'})
+            const imageURL = imageUpload.secure_url
+
+            await userModel.findByIdAndUpdate(userId,{image:imageURL})
+        }
+
+        res.json({success:true,message:"Profile Updated!"})
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+export { registerUser, loginUser, getProfile, updateProfile }

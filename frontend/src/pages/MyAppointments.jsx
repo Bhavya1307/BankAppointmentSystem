9fc6cbbb03873dd0bf99dbd75b810355b4daf7cf
Bhavya <bdp9834@gmail.com>
2024-10-07 21:19:00 -0400

    Added appointment api to frontend

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,26 +1,58 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { AppContext } from '../context/AppContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const MyAppointments = () => {
 
-  const { employees } = useContext(AppContext)
+  const { backendUrl, token } = useContext(AppContext)
+
+  const [appointments, setAppointmets] = useState([])
+  const months = ["", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
+  const slotDateFormat = (slotDate) => {
+    const dateArray = slotDate.split('-')
+    return dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
+  }
+
+  const getUserAppointments = async () => {
+    try {
+      
+      const {data} = await axios.get(backendUrl + '/api/user/appointments', {headers:{token}})
+
+      if (data.success) {
+        setAppointmets(data.appointments.reverse())
+        console.log(data.appointments);
+      }
+
+    } catch (error) {
+      console.log(error);
+      toast.error(error.message)
+    }
+  }
+
+  useEffect(()=>{
+    if (token) {
+      getUserAppointments()
+    }
+  },[token])
 
   return (
     <div>
       <p className='pb-3 mt-12 font-medium text-zinc-700 border-b'>My appointments</p>
       <div>
-        {employees.slice(0,3).map((item,index)=>(
+        {appointments.map((item,index)=>(
           <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
             <div>
-              <img className='w-32 bg-green-50 border border-green-200 rounded' src={item.image} alt="" />
+              <img className='w-32 bg-green-50 border border-green-200 rounded' src={item.empData.image} alt="" />
             </div>
             <div className='flex-1 text-sm text-zinc-600'>
-              <p className='text-neutral-800 font-semibold'>{item.name}</p>
-              <p>{item.position}</p>
+              <p className='text-neutral-800 font-semibold'>{item.empData.name}</p>
+              <p>{item.empData.position}</p>
               <p className='text-zinc-700 font-medium mt-1'>Address:</p>
-              <p className='text-xs'>{item.address.line1}</p>
-              <p className='text-xs'>{item.address.line2}</p>
-              <p className='text-sm mt'><span className='text-sm text-neutral-700 font-medium'>Date & Time:</span> 5 October, 2024 | 8:30 AM</p>
+              <p className='text-xs'>{item.empData.address.line1}</p>
+              <p className='text-xs'>{item.empData.address.line2}</p>
+              <p className='text-sm mt-2'><span className='text-sm text-neutral-700 font-medium'>Date & Time:</span> {slotDateFormat(item.slotDate)} | {item.slotTime}</p>
             </div>
             <div></div>
             <div className='flex flex-col gap-2 justify-center'>

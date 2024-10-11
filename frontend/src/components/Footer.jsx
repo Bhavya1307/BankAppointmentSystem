184ccc7790a41df7c7e69cc092b54ed47b34ca0e
Bhavya <bdp9834@gmail.com>
2024-10-11 14:01:05 -0400

    Added links in footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { NavLink } from 'react-router-dom'
 
 const Footer = () => {
   return (
@@ -8,17 +9,17 @@ const Footer = () => {
         {/* Left */}
         <div>
             <img className='mb-5 w-40' src={assets.logo} alt="A logo with a bank icon and a text called BankBuddy in green shades" />
-            <p className='w-full md:w-2/3 text-gray-600 leading-6'>Simply browse through our extensive list of trusted members, schedule your appointment hustle-free.</p>
+            <p className='w-full md:w-2/3 text-gray-600 leading-6'>Experience seamless banking with our expert staff. Schedule your appointment today and let us help you achieve your financial goals.</p>
         </div>
 
         {/* Center */}
         <div>
             <p className='text-xl font-medium mb-5'>COMPANY</p>
             <ul className='flex flex-col gap-2 text-gray-600'>
-                <li>Home</li>
-                <li>About us</li>
-                <li>Contact</li>
-                <li>Privacy policy</li>
+              <NavLink to = '/'><li>Home</li></NavLink>
+              <NavLink to = '/about'><li>About us</li></NavLink>
+              <NavLink to = '/contact'><li>Contact</li></NavLink>
+              <li>Privacy policy</li>
             </ul>
         </div>
 
